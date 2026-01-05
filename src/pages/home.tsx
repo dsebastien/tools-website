9fd2e01d89348b8b2da3fa34ffe36c15d2b39337
Sébastien Dubois <lechtitseb@gmail.com>
2026-01-05 13:39:59 +0100

    feat(all): improve ui on large screens

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -355,7 +355,7 @@ const HomePage: React.FC = () => {
                     <div
                         className={
                             viewMode === 'grid'
-                                ? 'grid gap-6 sm:grid-cols-2 lg:grid-cols-3'
+                                ? 'grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'
                                 : 'flex flex-col gap-3'
                         }
                     >
