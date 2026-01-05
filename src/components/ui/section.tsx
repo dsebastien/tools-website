97bbc87f63a3b4a30aea8c4fa655cdeae54505e1
Sébastien Dubois <lechtitseb@gmail.com>
2026-01-05 13:39:59 +0100

    feat(all): improve ui on large screens

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -13,7 +13,8 @@ const Section: React.FC<SectionProps> = ({ children, className, fullWidth = fals
             id={id}
             className={cn(
                 'xg:py-24 py-12 sm:py-14 md:py-16 lg:py-20 xl:py-28',
-                !fullWidth && 'xg:px-24 mx-auto max-w-7xl px-6 sm:px-10 md:px-16 lg:px-20 xl:px-32',
+                !fullWidth &&
+                    'xg:px-24 mx-auto max-w-[1600px] px-6 sm:px-10 md:px-16 lg:px-20 xl:px-32',
                 className
             )}
         >
